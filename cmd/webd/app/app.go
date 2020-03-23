56ccdbfe7b2ca64e250fb26085aeb602b8856f8b
Yash <yashshah96@gmail.com>
2020-03-22 20:36:09 -0400

    following/follower changes and added test cases

--- cmd/webd/app/app.go
+++ cmd/webd/app/app.go
@@ -14,8 +14,8 @@ type User struct {
 	Email     string
 	Password  string
 	id        uint64
-	following []*User
-	followers []*User
+	following map[uint64]uint64
+	followers map[uint64]uint64
 	post      []*Post
 }
 
@@ -54,21 +54,20 @@ func (appList *App) FollowUser(followingUserID uint64, UserIDToFollow uint64) {
 	appList.users[followingUserID].mu.Lock()
 	//Add userID to be followed in the following list of user who wants to follow
 	followingUserIDObject := appList.users[followingUserID]
-	following := followingUserIDObject.following
-	newfollowing := append(following, appList.users[UserIDToFollow])
+	newfollowing := followingUserIDObject.following
+	newfollowing[UserIDToFollow] = UserIDToFollow
 	followingUserIDObject.following = newfollowing
 	appList.users[followingUserID].mu.Unlock()
 
 	appList.users[UserIDToFollow].mu.Lock()
 	//Add userID who is following in the followers list of the user being followed
 	UserIDToFollowObject := appList.users[UserIDToFollow]
-	followers := UserIDToFollowObject.followers
-	newfollowers := append(followers, appList.users[followingUserID])
+	newfollowers := UserIDToFollowObject.followers
+	newfollowers[followingUserID] = followingUserID
 	UserIDToFollowObject.followers = newfollowers
 	appList.users[UserIDToFollow].mu.Unlock()
 }
 
-/*
 func (appList *App) UnFollowUser(followingUserID uint64, UserIDToUnfollow uint64) {
 	appList.usersMu.Lock()
 	defer appList.usersMu.Unlock()
@@ -76,34 +75,19 @@ func (appList *App) UnFollowUser(followingUserID uint64, UserIDToUnfollow uint64
 	appList.users[followingUserID].mu.Lock()
 	//Remove userID to be unfollowed from the following list of the user initiating unfollow request
 	followingUserIDObject := appList.users[followingUserID]
-	following := followingUserIDObject.following
-	newfollowing := GetUpdatedList(following, UserIDToUnfollow)
-	if newfollowing {
-		followingUserIDObject.following = newfollowing
-	}
+	newfollowing := followingUserIDObject.following
+	delete(newfollowing, UserIDToUnfollow)
+	followingUserIDObject.following = newfollowing
 	appList.users[followingUserID].mu.Unlock()
 
 	appList.users[UserIDToUnfollow].mu.Lock()
 	//Remove userID who is initiating the unfollow request from the followers list of the user being unfollowed
 	UserIDToUnfollowObject := appList.users[UserIDToUnfollow]
-	followers := UserIDToUnfollowObject.followers
-	newfollowers := GetUpdatedList(followers, followingUserID)
-	if newfollowers {
-		UserIDToUnfollowObject.followers = newfollowers
-	}
+	newfollowers := UserIDToUnfollowObject.followers
+	delete(newfollowers, followingUserID)
+	UserIDToUnfollowObject.followers = newfollowers
 	appList.users[UserIDToUnfollow].mu.Unlock()
 }
-
-func GetUpdatedList(appList []*User, UserID uint64) []*User {
-	var updatedList []*User
-	for userIDIndex := range appList {
-		if appList[userIDIndex].id == UserID {
-			updatedList = append(appList[:userIDIndex], appList[userIDIndex+1:])
-		}
-
-		return updatedList
-	}
-} */
 
 func (appList *App) CreatePost(userID uint64, message string) error {
 	currTime := time.Now()
@@ -123,7 +107,7 @@ func (appList *App) CreatePost(userID uint64, message string) error {
 }
 
 func MakeUser(firstname string, lastname string, email string, password string, id uint64) *User {
-	return &User{sync.Mutex{}, firstname, lastname, email, password, id, make([]*User, 0, 10), make([]*User, 0, 10), make([]*Post, 0, 10)}
+	return &User{sync.Mutex{}, firstname, lastname, email, password, id, make(map[uint64]uint64), make(map[uint64]uint64), make([]*Post, 0, 10)}
 }
 
 func (appList *App) AddUser(firstname string, lastname string, email string, password string) (uint64, error) {
@@ -145,7 +129,7 @@ func (appList *App) GetFeed(userId uint64) ([]*Post, error) {
 	posts := make([]*Post, 0, 100)
 	posts = append(posts, appList.users[userId].post...)
 	for _, v := range appList.users[userId].following {
-		posts = append(posts, v.post...)
+		posts = append(posts, appList.users[v].post...)
 	}
 	return posts, nil
 }
--- cmd/webd/app/app_test.go
+++ cmd/webd/app/app_test.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -9,11 +10,89 @@ func TestAddUser(t *testing.T) {
 	app := MakeApp()
 	expected := MakeUser("TestFirst", "TestLast", "test@test.com", "testpass", 0)
 	app.AddUser("TestFirst", "TestLast", "test@test.com", "testpass")
-	actual := app.getUsers()[0]
+	actual := app.GetUsers()[0]
 	if expected.Email != actual.Email ||
 		expected.FirstName != actual.FirstName ||
 		expected.LastName != actual.LastName ||
 		expected.id != actual.id {
 		t.Error(fmt.Sprintf("Test Failed: %s, %s, %s, %d", actual.Email, actual.FirstName, actual.LastName, actual.id))
+	}
+}
+
+func TestFollowUser(t *testing.T) {
+	app := MakeApp()
+	app.AddUser("FollowerFirst", "FollowerLast", "follower@test.com", "followerpass")
+	app.AddUser("FollowingFirst", "FollowingLast", "following@test.com", "followingpass")
+	followingUserID := app.users[0].id
+	UserIDToFollow := app.users[1].id
+	app.FollowUser(followingUserID, UserIDToFollow)
+	if app.users[0].following[UserIDToFollow] != UserIDToFollow {
+		t.Error("Test Failed Following map not updated properly")
+	}
+	if app.users[1].followers[followingUserID] != followingUserID {
+		t.Error("Test Failed Followers map not updated properly")
+	}
+
+}
+
+func TestUnFollowUser(t *testing.T) {
+	app := MakeApp()
+	app.AddUser("User0First", "User0Last", "User0@test.com", "User0pass")
+	app.AddUser("User1First", "User1Last", "User1@test.com", "User1gpass")
+	app.AddUser("User2First", "User2Last", "User2@test.com", "User2gpass")
+
+	User0FollowingList := map[uint64]uint64{2: 2}
+	User1FollowerList := make(map[uint64]uint64)
+	User2FollowerList := map[uint64]uint64{0: 0}
+
+	User0ID := app.users[0].id
+	User1ID := app.users[1].id
+	User2ID := app.users[2].id
+	app.FollowUser(User0ID, User1ID)
+	app.FollowUser(User0ID, User2ID)
+	app.UnFollowUser(User0ID, User1ID)
+
+	if reflect.DeepEqual(app.users[0].following, User0FollowingList) == false {
+		t.Error("Test Failed Following map not updated properly")
+	}
+	if reflect.DeepEqual(app.users[1].followers, User1FollowerList) == false {
+		t.Error("Test Failed Followers map not updated properly for User 1")
+	}
+	if reflect.DeepEqual(app.users[2].followers, User2FollowerList) == false {
+		t.Error("Test Failed Followers map not updated properly User 2")
+	}
+}
+
+func TestCreatePost(t *testing.T) {
+	app := MakeApp()
+	app.AddUser("TestFirst", "TestLast", "Test@test.com", "Testpass")
+	userID := app.users[0].id
+	app.CreatePost(userID, "Test Message")
+	userPost := app.users[0].post[0]
+	appPost := app.posts[0]
+
+	if reflect.DeepEqual(userPost, appPost) == false {
+		t.Error("Test Failed User struct not updated properly for the added post")
+	}
+	if userPost.Message != "Test Message" {
+		t.Error("Test Failed User struct not updated properly for Post message ")
+	}
+	posts, _ := app.GetFeed(userID)
+	if reflect.DeepEqual(appPost, posts[0]) == false {
+		t.Error("Test Failed Posts struct not in sync")
+	}
+}
+
+func TestGetFeed(t *testing.T) {
+	app := MakeApp()
+	app.AddUser("TestFirst", "TestLast", "Test@test.com", "Testpass")
+	userID := app.users[0].id
+	app.CreatePost(userID, "Test Message")
+	userPost := app.users[0].post[0]
+
+	posts, _ := app.GetFeed(userID)
+
+	if reflect.DeepEqual(userPost, posts[0]) == false {
+		t.Error("Test Failed added post not returned in the list of feeds")
 	}
 }
