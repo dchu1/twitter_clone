5c386500aa3826371a8ab027a3b7fb1b1121ca65
David Chu <dfc296@nyu.edu>
2020-03-23 14:59:20 -0400

    Merged loginHandler conflict

--- cmd/webd/handlers/loginHandler.go
+++ cmd/webd/handlers/loginHandler.go
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-package handlers
-
-import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-
-	"github.com/Distributed-Systems-CSGY9223/yjs310-shs572-dfc296-final-project/cmd/webd/auth/session"
-
-	handlermodels "github.com/Distributed-Systems-CSGY9223/yjs310-shs572-dfc296-final-project/cmd/webd/handlers/models"
-)
-
-func Login(w http.ResponseWriter, r *http.Request) {
-	var user handlermodels.LoginRequest
-	body, err := ioutil.ReadAll(r.Body)
-	sess := session.GlobalSessions.SessionStart(w, r)
-	json.Unmarshal([]byte(body), &user)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	//code to check if user exists
-	if a.ValidateCredentials(user.Email, user.Password) {
-		sess.Set("username", user.Email)
-		sess.Set("authenticated", true)
-		APIResponse(w, r, http.StatusOK, "Login successful", make(map[string]string)) // send data to client side
-	} else {
-		APIResponse(w, r, http.StatusUnauthorized, "Login unsuccessful", make(map[string]string)) // send data to client side
-	}
-
-}
-=======
 package handlers
 
 import (
@@ -68,4 +35,3 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
->>>>>>> david
