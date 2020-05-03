5b27f2902d1c58389e06e8036fd1c6901b712550
smitsheth10@yahoo.com <smitsheth10@yahoo.com>
2020-05-03 17:54:33 -0400

    Add etcd connection in auth server

--- cmd/authd/main.go
+++ cmd/authd/main.go
@@ -7,6 +7,7 @@ import (
 
 	pb "github.com/Distributed-Systems-CSGY9223/yjs310-shs572-dfc296-final-project/internal/auth/authentication"
 	"github.com/Distributed-Systems-CSGY9223/yjs310-shs572-dfc296-final-project/internal/auth/service"
+	etcd "github.com/Distributed-Systems-CSGY9223/yjs310-shs572-dfc296-final-project/internal/auth/storage/etcd"
 	"github.com/Distributed-Systems-CSGY9223/yjs310-shs572-dfc296-final-project/internal/config"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
@@ -16,6 +17,7 @@ func main() {
 	// Read Config
 	runtimeViper := viper.New()
 	config.NewRuntimeConfig(runtimeViper, ".")
+	storage := "etcd"
 
 	// Start server
 	lis, err := net.Listen("tcp", "localhost:"+runtimeViper.GetStringSlice("authservice.ports")[0])
@@ -24,7 +26,13 @@ func main() {
 	}
 	s := grpc.NewServer()
 	fmt.Println("Server running on port", "localhost:"+runtimeViper.GetStringSlice("authservice.ports")[0])
-	pb.RegisterAuthenticationServer(s, service.GetAuthServer())
+	if storage == "etcd" {
+		client, _ := etcd.NewClient([]string{"http://localhost:2379"})
+		pb.RegisterAuthenticationServer(s, service.GetEtcdAuthServer(client))
+	} else {
+		pb.RegisterAuthenticationServer(s, service.GetAuthServer())
+	}
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
